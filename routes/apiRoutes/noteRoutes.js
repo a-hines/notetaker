a011f5ee2167971ade74c7aabd0a92f6a889fb19
Alexa Hines <99751726+a-hines@users.noreply.github.com>
2022-11-05 16:32:00 -0400

    set up routes for css and js

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const notes = require('../../db/db.json');
+const { notes } = require('../../db/db.json');
 const createNewNote = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
@@ -12,6 +12,7 @@ router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
 
     const note = createNewNote(req.body, notes);
+    
     res.json(note);
 })
 
