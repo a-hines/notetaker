b5fb4460721d9a298fe8de62a727611cef220bed
Alexa Hines <99751726+a-hines@users.noreply.github.com>
2022-11-05 15:30:03 -0400

    created post route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
 
 const notes = require('../../db/db.json');
+const createNewNote = require('../../lib/notes');
 
-router.get('/db', (req, res) => {
+router.get('/notes', (req, res) => {
     let saved = notes;
     res.json(saved);
-});
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+})
 
 module.exports = router;
